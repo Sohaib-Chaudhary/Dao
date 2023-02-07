ecec4f3096cb0bed258ea1907955c993f0b0bee0
Sohaib Chaudhary <sohaibchoudhary721@gmail.com>
2023-02-08 02:21:00 +0500

    add hero section

--- daowebsite/src/app/page.tsx
+++ daowebsite/src/app/page.tsx
@@ -2,13 +2,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 
 import NavBar from './navbar'
-
+import Hero from './Hero'
 
 
 export default function Home() {
   return (
     <ChakraProvider>
       <NavBar />
+      <Hero/>
     </ChakraProvider>
   )
 }
