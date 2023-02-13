9cf8778fb07ebe6e268fccbb7cfd5899fedc6e96
Sohaib Chaudhary <sohaibchoudhary721@gmail.com>
2023-02-13 05:40:40 +0500

    remove navbar

--- coursesapp/pages/index.tsx
+++ coursesapp/pages/index.tsx
@@ -2,7 +2,7 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import { ChakraProvider } from '@chakra-ui/react'
-import NabBar from './NavBar'
+
 const inter = Inter({ subsets: ['latin'] })
 import Hero from './Hero'
 export default function Home() {
@@ -15,7 +15,7 @@ export default function Home() {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <NabBar />
+      
         <Hero />
       </ChakraProvider>
 
